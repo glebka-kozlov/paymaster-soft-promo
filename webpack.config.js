94b0b514cbc599867c0dec2532968a1017d60968
glebka_kozlov <glebkakozlov1996@gmail.com>
2024-03-14 19:57:31 +0300

    develop

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = (env, argv) => {
     entry: "./src/index.js",
     output: {
       filename: argv.mode === "development" ? "[name].js" : "[name].[fullhash].js",
-      path: argv.mode === "development" ? path.resolve(__dirname, "dev") : path.resolve(__dirname, "dist"),
+      path: argv.mode === "development" ? path.resolve(__dirname, "dev") : path.resolve(__dirname, "promo"),
       chunkFilename: "[name].[fullhash].js"
     },
     module: {
